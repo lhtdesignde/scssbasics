df16f12588d57dd973c574126fd497c964bd86c9
Stefanie Schneider <info@lhtdesign.de>
2015-03-14 18:46:50 -0700

    updated classes to hyphens instead of underscore, added new module to init module use, updated html, updated browser minimums, changed timestamp to asctime, removed some mot used helper classes, updated variables, made main comment unremovable by compression

--- config.rb
+++ config.rb
@@ -2,13 +2,18 @@
 module Sass::Script::Functions
     # put timestamp into css file, request with #{timestamp()}
     def timestamp()
-        return Sass::Script::String.new(Time.now.to_s)
+        return Sass::Script::String.new(Time.now.asctime)
     end
 end
 
 
 # Require any additional compass plugins:
-# require "sass-globbing"
+
+# Sass globbing allows you to import many sass or scss files in a single import statement
+# https://github.com/chriseppstein/sass-globbing
+# used in style.scss, mod included
+require "sass-globbing"
+
 
 # Set this to the root of your project when deployed:
 http_path = "static/css"
@@ -18,14 +23,18 @@ images_dir = "images"
 javascripts_dir = "js"
 fonts_dir = "fonts"
 
+
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :expanded #:expanded or :nested or :compact or :compressed
+
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
+
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
+
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
